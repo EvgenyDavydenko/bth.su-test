b178c9c8e64f91c14bd2a0b2c329e290d63ad222
Evgeny Davydenko <frontend82@mail.ru>
2023-12-10 00:41:11 +0300

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Notifications\Notifiable;
 
 class Product extends Model
 {
-    use HasFactory, Notifiable;
+    use SoftDeletes, HasFactory, Notifiable;
 
     protected $table = 'products';
 
